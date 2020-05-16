5e89157a089784daf03bf348e6d97907998e1713
Leonardo Garruba <leonardogarrubaaraujo@gamil.com>
2020-05-16 13:12:03 -0300

    Alteração na biblioteca MySQL

---
+++ src/database/CodeBros-dumpfile.sql
@@ -0,0 +1,146 @@
+CREATE DATABASE  IF NOT EXISTS `CodeBros` /*!40100 DEFAULT CHARACTER SET utf8 */ /*!80016 DEFAULT ENCRYPTION='N' */;
+USE `CodeBros`;
+-- MySQL dump 10.13  Distrib 8.0.20, for Win64 (x86_64)
+--
+-- Host: 127.0.0.1    Database: CodeBros
+-- ------------------------------------------------------
+-- Server version	8.0.20
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `emprestimo`
+--
+
+DROP TABLE IF EXISTS `emprestimo`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `emprestimo` (
+  `idemprestimo` int NOT NULL AUTO_INCREMENT,
+  `CPF` varchar(15) NOT NULL,
+  `idlivro` int NOT NULL,
+  `data_emprestimo` datetime DEFAULT NULL,
+  `data_devolucao` datetime DEFAULT NULL,
+  PRIMARY KEY (`idemprestimo`),
+  KEY `CPF` (`CPF`),
+  KEY `idlivro` (`idlivro`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `emprestimo`
+--
+
+LOCK TABLES `emprestimo` WRITE;
+/*!40000 ALTER TABLE `emprestimo` DISABLE KEYS */;
+INSERT INTO `emprestimo` VALUES (1,'123',1,'2020-05-13 16:10:56','2020-05-13 16:10:56');
+/*!40000 ALTER TABLE `emprestimo` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `livro`
+--
+
+DROP TABLE IF EXISTS `livro`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `livro` (
+  `idlivro` int NOT NULL,
+  `titulo` varchar(50) DEFAULT NULL,
+  `nome_do_autor` varchar(50) DEFAULT NULL,
+  `editora` varchar(50) DEFAULT NULL,
+  `ano` int DEFAULT NULL,
+  `status` varchar(15) DEFAULT (_utf8mb4'Disponível'),
+  PRIMARY KEY (`idlivro`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `livro`
+--
+
+LOCK TABLES `livro` WRITE;
+/*!40000 ALTER TABLE `livro` DISABLE KEYS */;
+INSERT INTO `livro` VALUES (1,'Teste','João','Editora Teste',2020,'Emprestado');
+/*!40000 ALTER TABLE `livro` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `usuario`
+--
+
+DROP TABLE IF EXISTS `usuario`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `usuario` (
+  `CPF` varchar(15) NOT NULL,
+  `nome` varchar(50) DEFAULT NULL,
+  `idade` int DEFAULT NULL,
+  `endereço` varchar(50) DEFAULT NULL,
+  `sexo` varchar(15) DEFAULT NULL,
+  PRIMARY KEY (`CPF`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `usuario`
+--
+
+LOCK TABLES `usuario` WRITE;
+/*!40000 ALTER TABLE `usuario` DISABLE KEYS */;
+INSERT INTO `usuario` VALUES ('123','Leonardo',20,'Rua 10','Masculino');
+/*!40000 ALTER TABLE `usuario` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Temporary view structure for view `v_emprestimo`
+--
+
+DROP TABLE IF EXISTS `v_emprestimo`;
+/*!50001 DROP VIEW IF EXISTS `v_emprestimo`*/;
+SET @saved_cs_client     = @@character_set_client;
+/*!50503 SET character_set_client = utf8mb4 */;
+/*!50001 CREATE VIEW `v_emprestimo` AS SELECT 
+ 1 AS `CPF`,
+ 1 AS `nome`,
+ 1 AS `idlivro`,
+ 1 AS `titulo`*/;
+SET character_set_client = @saved_cs_client;
+
+--
+-- Final view structure for view `v_emprestimo`
+--
+
+/*!50001 DROP VIEW IF EXISTS `v_emprestimo`*/;
+/*!50001 SET @saved_cs_client          = @@character_set_client */;
+/*!50001 SET @saved_cs_results         = @@character_set_results */;
+/*!50001 SET @saved_col_connection     = @@collation_connection */;
+/*!50001 SET character_set_client      = utf8mb4 */;
+/*!50001 SET character_set_results     = utf8mb4 */;
+/*!50001 SET collation_connection      = utf8mb4_0900_ai_ci */;
+/*!50001 CREATE ALGORITHM=UNDEFINED */
+/*!50013 DEFINER=`root`@`localhost` SQL SECURITY DEFINER */
+/*!50001 VIEW `v_emprestimo` AS select `u`.`CPF` AS `CPF`,`u`.`nome` AS `nome`,`l`.`idlivro` AS `idlivro`,`l`.`titulo` AS `titulo` from ((`usuario` `u` join `livro` `l`) join `emprestimo` `e`) where ((`e`.`idlivro` = `l`.`idlivro`) and (`e`.`CPF` = `u`.`CPF`)) */;
+/*!50001 SET character_set_client      = @saved_cs_client */;
+/*!50001 SET character_set_results     = @saved_cs_results */;
+/*!50001 SET collation_connection      = @saved_col_connection */;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2020-05-13 11:50:57
